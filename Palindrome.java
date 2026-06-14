083057b0845d309894df0832553f2c397a74bf40
Rohit Patil <patilrohit172004@gmail.com>
2026-06-14 19:58:13 +0530

    Code: Add working Java solution for Palindrome Number

--- Palindrome.java
+++ Palindrome.java
@@ -1,1 +1,24 @@
 // Solution for Palindrome Numbers
+
+class Solution {
+    public boolean isPalindrome(int x) {
+
+    // Check Wheather X should greater than '0' or not
+        if(x < 0){
+            return false;
+        }
+
+        int og = x;
+        int rev = 0;
+        
+    // Reverse Integer Logic
+        while(x != 0){
+            int y = x % 10;
+            rev = rev * 10 + y;
+            x = x / 10;
+        }
+
+            return og == rev;
+    }
+  
+}
